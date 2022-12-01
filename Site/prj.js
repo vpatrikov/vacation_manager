d296507681ca5b2dd589d0501c7112110c5ad0c9
IliyanIvanov23 <72295347+IliyanIvanov23@users.noreply.github.com>
2022-12-01 20:28:20 +0200

    bruh didnt save

--- Site/prj.js
+++ Site/prj.js
@@ -2,31 +2,35 @@ const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const http = require('http');
 let db = new sqlite3.Database('../Database.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Conn Succesful!');
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Conn Succesful!');
 });
-/*console.log("VM started");
-let sql = `SELECT * 
+
+db.close();
+//console.log("VM started");
+function selectAllFromTeam(id) {
+	let sql = `SELECT * 
             FROM Users 
             WHERE team = ?`;
-db.each(sql, [4], (err, row) => {
-    if (err) {
-        throw err;
-    };
-    console.log(`${row.username}: ${row.fname} ${row.lname}`);
-});*/
-deleteAtId(51);
-db.close();
+	db.each(sql, [id], (err, row) => {
+		if (err) {
+			throw err;
+		};
+		console.log(`${row.username}: ${row.fname} ${row.lname}`);
+	});
+}
+
+
 function deleteAtId(id) {
-    let sql = 'DELETE FROM Users WHERE id = ?';
-    //let table = prompt('Please enter name of table');
-    //let id = prompt('Please enter Id you want to delete at');
-    db.run(sql, [id], (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`Successfuly deleted at ${id} from Users`);
-    });
+	let sql = 'DELETE FROM Users WHERE id = ?';
+	//let table = prompt('Please enter name of table');
+	//let id = prompt('Please enter Id you want to delete at');
+	db.run(sql, [id], (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`Successfuly deleted at ${id} from Users`);
+	});
 }
