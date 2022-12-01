10b2c1f4b9abaeebad7d96835eaba13cb9d54d25
IliyanIvanov23 <72295347+IliyanIvanov23@users.noreply.github.com>
2022-12-01 20:22:45 +0200

    Started work on CRUD Ops for Users Table
    Created select * query for users using teamid as argument and remove query using id as argument

--- Site/prj.js
+++ Site/prj.js
@@ -1,9 +1,32 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const http = require('http');
-let db = new sqlite3.Database('../Database.db');
-console.log("VM started");
-console.log(db.run('SELECT * FROM Users where team = 4'));
-
-
+let db = new sqlite3.Database('../Database.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Conn Succesful!');
+});
+/*console.log("VM started");
+let sql = `SELECT * 
+            FROM Users 
+            WHERE team = ?`;
+db.each(sql, [4], (err, row) => {
+    if (err) {
+        throw err;
+    };
+    console.log(`${row.username}: ${row.fname} ${row.lname}`);
+});*/
+deleteAtId(51);
 db.close();
+function deleteAtId(id) {
+    let sql = 'DELETE FROM Users WHERE id = ?';
+    //let table = prompt('Please enter name of table');
+    //let id = prompt('Please enter Id you want to delete at');
+    db.run(sql, [id], (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`Successfuly deleted at ${id} from Users`);
+    });
+}
