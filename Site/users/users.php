08f43a1c75d2262ee331d22315d5ccb9435526ac
Vladimir Patrikov <vladimir.n.patrikov@gmail.com>
2023-02-15 12:21:20 +0200

    Added filters

--- Site/users/users.php
+++ Site/users/users.php
@@ -15,6 +15,7 @@
         <h1>Users</h1>
         <?php
         session_start();
+<<<<<<< Updated upstream
         if ($_SESSION['role'] == 'CEO') {
         ?>
             <nav>
@@ -46,8 +47,23 @@
         ?>
         <?php
         echo "<br><table border = 1>";
+=======
 
-        echo "
+        function print_data($sql_query)
+        {
+>>>>>>> Stashed changes
+
+            $db = new PDO("sqlite:../../Database.db");
+            $statement = $db->query($sql_query);
+            $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            if (count($users) == 0) {
+                echo "<p class='invalid'>Invalid input</p>";
+                return;
+            } else {
+                echo "<br><table border = 1>";
+
+                echo "
             <tr>
                 <td><b>ID<b></td>
                 <td><b>Username<b></td>
@@ -59,23 +75,88 @@
                 <td><b>Delete<b></td>
             </tr>";
 
-        foreach ($users as $row => $data) {
-            echo "<tr>";
-            echo "<td>" . $data['id'] . "." . "</td>";
-            echo "<td>" . $data['username'] . "</td>";
-            echo "<td>" . $data['pass'] . "</td>";
-            echo "<td>" . $data['fname'] . "</td>";
-            echo "<td>" . $data['lname'] . "</td>";
-            echo "<td>" . $data['role'] . "</td>";
-            echo "<td>" . $data['team'] . "</td>";
-            echo "<td align='center'>" ?> <a class='a_links' href="users_delete.php?id=<?php echo $data['id']; ?>">Delete</a></td>
-        <?php
-            echo "</tr>";
-        }
+                foreach ($users as $row => $data) {
+                    echo "<tr>";
+                    echo "<td>" . $data['id'] . "." . "</td>";
+                    echo "<td>" . $data['username'] . "</td>";
+                    echo "<td>" . $data['pass'] . "</td>";
+                    echo "<td>" . $data['fname'] . "</td>";
+                    echo "<td>" . $data['lname'] . "</td>";
+                    echo "<td>" . $data['role'] . "</td>";
+                    echo "<td>" . $data['team'] . "</td>";
+                    echo "<td align='center'>" ?> <a class='a_links' href="users_delete.php?id=<?php echo $data['id']; ?>">Delete</a></td>
+            <?php
+                    echo "</tr>";
+                }
 
+<<<<<<< Updated upstream
         echo "</table>";
         ?>
     </div>
+=======
+                echo "</table>";
+            }
+        }
+
+        if ($_SESSION['role'] == 'CEO') {
+            ?>
+            <nav>
+                <a href="../index.php">Home</a>
+                <a href="../teams/teams.php">Teams</a>
+                <a href="../projects/projects.php">Projects</a>
+                <a href="../vacations/vacations.php">Vacations</a>
+                <a href="../users/users.php">Users</a>
+            </nav>
+        <?php
+        } else {
+        ?>
+            <nav>
+                <a href="../index.php">Home</a>
+                <a href="../teams/teams.php">Teams</a>
+                <a href="../projects/projects.php">Projects</a>
+                <a href="../vacations/vacations.php">Vacations</a>
+            </nav>
+        <?php
+        }
+        ?>
+    </header>
+    <form class="div_align scrollabe" method="post">
+        <input class="textboxes_filter" style="width:17%;" type="text" name=filter_username placeholder="Filter by username:">
+
+        <input class="textboxes_filter" style="width:17%;" type="text" name=filter_fname placeholder="Filter by first name:">
+
+        <input class="textboxes_filter" style="width:17%;" type="text" name=filter_lname placeholder="Filter by last name:">
+
+        <input class="textboxes_filter" type="text" style="width:12%;" name=filter_role placeholder="Filter by role:"><br><br>
+
+        <input class="actionbttns" type="submit" name="filter" value="Filter">
+    </form>
+    <?php
+    if (!isset($_POST['filter'])) {
+        print_data("SELECT * FROM Users");
+    } else {
+        $username = $_POST['filter_username'];
+        $fname = $_POST["filter_fname"];
+        $lname = $_POST["filter_lname"];
+        $role = $_POST["filter_role"];
+
+        $query = "SELECT * FROM Users";
+
+       if ($fname) {
+            $query .= " WHERE fname = '$fname'";
+        } else if ($lname) {
+            $query .= " WHERE lname = '$lname'";
+        } else if ($username) {
+
+            $query .= " WHERE username = '$username'";
+        } else if ($role) {
+            $query .= " WHERE role = '$role'";
+        }
+
+        print_data($query);
+    }
+    ?>
+>>>>>>> Stashed changes
 </body>
 
 </html>
